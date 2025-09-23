b48691bdd61cfc35a6395842348a838a75ec0b2e
Asim <asimokon@yahoo.com>
2025-09-23 08:59:07 +0100

    first commit

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
