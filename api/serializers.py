db6bd8b13dd81d78acc32a8d78418fa3380d7166
Asim <asimokon@yahoo.com>
2025-09-28 10:15:52 +0100

    added order and orderitems

--- api/serializers.py
+++ api/serializers.py
@@ -35,7 +35,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
             'product_price',
             'quantity',
             'item_subtotal'
-        )
+        ) 
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
